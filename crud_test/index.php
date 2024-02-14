670ecfcfd27e65a6b1630a3fe3b00fff9cb831ff
Siwanon1234 <124326792+Siwanon1234@users.noreply.github.com>
2024-02-14 11:08:32 +0700

    V.2
    เพิ่ม CSS

--- crud_test/index.php
+++ crud_test/index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php require_once "connect.php" ?>
+    <?php require_once "setHead.php" ?>
 </head>
 <body>
-    
+    <H1 class="container mt-5"> นักเรียนไม่ตั้งใจเรียน</H1>
 </body>
+    <?php require_once "setFoot.php" ?>
 </html>
